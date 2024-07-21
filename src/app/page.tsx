b05dced1d7f75699ec03508b8e20fee4970b0843
NoxSensei <noxmati@gmail.com>
2024-07-21 15:38:22 +0200

    feat: add drawing grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,157 @@
-import Image from "next/image";
+'use client'
+
+import {useEffect, useState} from "react";
+import {Stage, Layer, Rect, Line} from 'react-konva';
+
+const rowsCount = 3;
+const columnsCount = 3;
+
+class Node {
+    isWallOnTop = true;
+    isWallOnRight = true;
+    isWallOnBot = true;
+    isWallOnLeft = true;
+}
+
+
+const grid = Array.from(new Array(rowsCount), () => new Array(columnsCount).fill(undefined).map(() => new Node()))
+
+const directions = ['N', 'E', 'S', 'W']
+
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+function shuffleArray(array) {
+    const a = Array.from(array);
+    return a
+        .map(value => ({value, sort: Math.random()}))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({value}) => value)
+}
+
+function generateMaze() {
+    const visited = new Set();
+    const i = randomInt(0, rowsCount - 1);
+    const j = randomInt(0, columnsCount - 1);
+
+    backtracer(i, j, visited)
+}
+
+function backtracer(i, j, visited) {
+    visited.add(`${i}_${j}`);
+    shuffleArray(directions).forEach(direction => {
+        if (direction === 'N') {
+            const index = i - 1;
+            if (index < 0) {
+                return;
+            }
+            if (visited.has(`${index}_${j}`)) {
+                return;
+            }
+
+            grid[index][j].isWallOnBot = false;
+            grid[i][j].isWallOnTop = false;
+            backtracer(index, j, visited)
+        } else if (direction === 'E') {
+            const index = j + 1;
+            if (index > columnsCount - 1) {
+                return;
+            }
+            if (visited.has(`${i}_${index}`)) {
+                return;
+            }
+            grid[i][index].isWallOnLeft = false;
+            grid[i][j].isWallOnRight = false;
+            backtracer(i, index, visited)
+        } else if (direction === 'S') {
+            const index = i + 1;
+            if (index > rowsCount - 1) {
+                return;
+            }
+            if (visited.has(`${index}_${j}`)) {
+                return;
+            }
+            grid[index][j].isWallOnTop = false;
+            grid[i][j].isWallOnBot = false;
+            backtracer(index, j, visited)
+        } else if (direction === 'W') {
+            const index = j - 1;
+            if (index < 0) {
+                return;
+            }
+            if (visited.has(`${i}_${index}`)) {
+                return;
+            }
+            grid[i][index].isWallOnRight = false;
+            grid[i][j].isWallOnLeft = false;
+            backtracer(i, index, visited)
+        }
+    })
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [nodes, setNodes] = useState([]);
+
+    useEffect(
+        () => {
+            generateMaze();
+            drawNodes();
+        },
+        []
+    );
+
+    function drawGrid() {
+        const gridStroke = 2;
+        const width = 400;
+        const height = 400;
+
+        const spaceX = width / rowsCount;
+        const spaceY = width / columnsCount;
+
+        const lines = [];
+        for (let i = 0; i < rowsCount + 1; i++) {
+            lines.push(<Line points={[0, i * spaceY, width, i * spaceY]} stroke="black" strokeWidth={gridStroke}/>)
+        }
+
+        for (let i = 0; i < columnsCount + 1; i++) {
+            lines.push(<Line points={[i * spaceX, 0, i * spaceX, height]} stroke="black" strokeWidth={gridStroke}/>)
+        }
+
+        return <Layer>{lines}</Layer>
+    }
+
+    function drawNodes() {
+        const width = 400;
+        const height = 400;
+
+        const spaceX = width / rowsCount;
+        const spaceY = height / columnsCount;
+
+        const nodes2 = [];
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                nodes2.push(<Rect x={spaceX * i}
+                                  y={spaceY * j}
+                                  width={spaceX}
+                                  height={spaceY}
+                                  fill='grey'/>)
+            }
+        }
+        setNodes(nodes2)
+    }
+
+    return (
+        <div style={{display: 'flex', justifyContent: "center", alignItems: 'center', textAlign: 'center'}}>
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                {
+                    drawGrid()
+                }
+                <Layer>
+                    {nodes}
+                </Layer>
+            </Stage>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
