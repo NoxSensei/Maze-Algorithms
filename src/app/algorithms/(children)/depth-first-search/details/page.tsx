450f86ec6b68e028f945308f8636545dbb1c5ff6
NoxSensei <noxmati@gmail.com>
2024-08-24 18:39:57 +0200

    feat: update spacing for depth first search details

--- src/app/algorithms/(children)/depth-first-search/details/page.tsx
+++ src/app/algorithms/(children)/depth-first-search/details/page.tsx
@@ -10,9 +10,9 @@ export default function P() {
             color: "black",
         }}>
         <div className='text-2xl text-center pb-3'>Depth First Search</div>
-        <div className="grid grid-cols-2 pt-10">
+        <div className="grid grid-cols-2 space-x-10 pt-10">
             <DepthFirstSearchDescriptionComponent/>
-            <div className="pt-5">
+            <div>
                 {/* TODO use static image */}
                 <DepthFirstSearchAlgorithmCanvasComponent dimension={10}/>
             </div>
