b2fcfe317437d977fc98ed85d6e0213e146e99f3
NoxSensei <noxmati@gmail.com>
2024-08-29 20:21:34 +0200

    feat: change grid size input from number to range type

--- src/app/algorithms/(children)/depth-first-search/creator/page.tsx
+++ src/app/algorithms/(children)/depth-first-search/creator/page.tsx
@@ -3,65 +3,92 @@
 import DepthFirstSearchAlgorithmCanvasComponent
     from "@/app/algorithms/(children)/depth-first-search/depth-first-search-canvas.component";
 import {ChangeEvent, useEffect, useRef, useState} from "react";
+import {faForwardStep, faForwardFast, faPlay, faPause} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function P() {
-    const gridSizeMin = 5;
-    const gridSizeMax = 50;
+    const minGridSize = 5;
+    const maxGridSize = 50;
+    const gridSizeStep = 5;
     const defaultGridSize = 15;
-    const [girdSizeInput, setGridSizeInput] = useState<string | number>('');
     const [gridSize, setGridSize] = useState<number>(defaultGridSize);
-
-    const gridSizeInputRef = useRef<HTMLInputElement | null>(null);
 
     useEffect(() => {
         console.log("reload")
     }, [gridSize]);
 
-    function gridSizeInputChanged(event: ChangeEvent<HTMLInputElement>) {
-        let value = event.target.value;
-        value = value.replaceAll(/\D/g, '')
-        setGridSizeInput(value);
-        if (!value) {
-            setInvalidInput('');
-            return;
-        }
-
-        if (value < gridSizeMin) {
-            setInvalidInput('');
-            return;
-        }
-
-        if (value > gridSizeMax) {
-            setInvalidInput('');
-            return;
-        }
-
-        const numericValue = Number.parseInt(value);
-        setGridSize(numericValue);
-        console.log("Grid size", gridSize)
-        setCorrectInput();
-    }
-
-    function setInvalidInput(reasons: string) {
-        gridSizeInputRef.current!.style.borderColor = 'red';
-        gridSizeInputRef.current!.style.outline = 'none'
-    }
-
-    function setCorrectInput() {
-        gridSizeInputRef.current!.style.borderColor = 'inherit';
-        gridSizeInputRef.current!.style.outline = 'inherit'
+    function onGridSizeChanged(event: ChangeEvent<HTMLInputElement>) {
+        const value = parseInt(event.target.value);
+        setGridSize(value);
     }
 
     return <div className="h-full pb-10 text-black">
-        <div className="flex justify-center items-center space-x-2 mb-2">
-            <label htmlFor="gridSize">Grid size</label>
-            <input ref={gridSizeInputRef}
-                   type="text"
-                   id="gridSize"
-                   placeholder={`${gridSizeMin}-${gridSizeMax}`}
-                   value={girdSizeInput}
-                   onChange={gridSizeInputChanged}
-                   className="border-2"/>
+        <div className="flex justify-center space-x-28">
+            <div className="flex flex-col space-y-2 p-2 w-80 h-20">
+                <input type="range" min={minGridSize} max={maxGridSize} step={gridSizeStep} defaultValue={gridSize} onChange={onGridSizeChanged}/>
+                <ul className="flex justify-between w-full px-[10px]">
+                    <li className="flex justify-center relative">
+                        <span className="absolute">5</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">10</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">15</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">20</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">25</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">30</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">35</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">40</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">45</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">50</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="flex justify-center space-x-2">
+                <button>
+                    <FontAwesomeIcon icon={faPause}/>
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faPlay}/>
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faForwardStep}/>
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faForwardFast}/>
+                </button>
+            </div>
+
+            <div className="flex flex-col space-y-2 p-2 w-80  h-20">
+                <input type="range" className="w-full" min="1" max="3" step="1"/>
+                <ul className="flex justify-between w-full px-[10px]">
+                    <li className="flex justify-center relative">
+                        <span className="absolute">Slow</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">Medium</span>
+                    </li>
+                    <li className="flex justify-center relative">
+                        <span className="absolute">Fast</span>
+                    </li>
+                </ul>
+            </div>
         </div>
         <DepthFirstSearchAlgorithmCanvasComponent dimension={gridSize}/>
     </div>
