fad339b388183a23a54d2b9fd21143ebe834a364
NoxSensei <noxmati@gmail.com>
2024-11-22 20:58:47 +0100

    build: add deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "Maze-Algorightms"
+    basePath: "/Maze-Algorightms"
 };
 
 export default nextConfig;
