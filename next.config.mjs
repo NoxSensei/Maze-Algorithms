1f1a636eb8a4a4417724d6725cdfc91bb0c3a2eb
NoxSensei <noxmati@gmail.com>
2024-11-23 00:42:30 +0100

    build: add deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     output: "export",
     basePath: process.env.PUBLIC_BASE_PATH ?? '',
-    assetPrefix: process.env.PUBLIC_BASE_PATH ?? ''
+    assetPrefix: process.env.PUBLIC_BASE_PATH ?? '',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
