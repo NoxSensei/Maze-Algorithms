4bd3691eebaff2596f273fb437cc622ee92c394b
NoxSensei <noxmati@gmail.com>
2024-11-24 18:53:46 +0100

    refactor: optimize and cleanup the eller algorithm

--- src/app/algorithms/eller/creator/_services/eller-algorithm.ts
+++ src/app/algorithms/eller/creator/_services/eller-algorithm.ts
@@ -8,106 +8,132 @@ class ExtendedMazeNode extends MazeNode {
     groupId?: string;
 }
 
+interface NodesGroup {
+    nodes: ExtendedMazeNode[]
+}
+
 export class EllerAlgorithm extends MazeAlgorithm {
     protected removeWalls(maze: Maze, visitedNodes: Set<MazeNode>): void {
-        for (const [rowIndex, row] of (maze.grid as ExtendedMazeNode[][]).entries()) {
-            row.forEach((node: ExtendedMazeNode) => {
-                if (!node.groupId) {
-                    node.groupId = v4()
-                }
-            });
+        const extendedMazeGrid = maze.grid as ExtendedMazeNode[][]
+        const nodesGroups = new Map<string, NodesGroup>();
+
+        for (const [rowIndex, row] of extendedMazeGrid.entries()) {
+            const currentRowNodesGroups = new Map<string, NodesGroup>();
+            const isLastRow = rowIndex === row.length - 1;
+
+            this.assignGroupIdForEachNodeInRow(row, nodesGroups);
 
             for (const [columnIndex, node] of row.entries()) {
                 visitedNodes.add(node);
 
-                const isLastColumn = columnIndex === row.length - 1;
-                if (isLastColumn) {
-                    continue;
-                }
-
-                const nodeOnTheEast = row[columnIndex + 1];
-                if (nodeOnTheEast.groupId === node.groupId) {
-                    continue;
-                }
-
-                const shouldDestroyWallOnEast = JsHelpers.isRandomChanceMet(0.5);
-                if (!shouldDestroyWallOnEast) {
-                    continue;
-                }
-
-                node.isWallOnEast = false;
-                nodeOnTheEast.isWallOnWest = false;
-
-                const oldid = nodeOnTheEast.groupId!;
-                nodeOnTheEast.groupId = node.groupId;
-                this.replace(maze, oldid, node.groupId!)
+                this.buildEasternPassages(nodesGroups, node, columnIndex, row, isLastRow);
+                this.addNodeToTheCurrentRowNodesGroup(currentRowNodesGroups, node);
             }
 
-
-            const isLastRow = rowIndex === row.length - 1;
+            // Do not destroy bottom walls if it is the last row
             if (isLastRow) {
-                for (const [columnIndex, node] of row.entries()) {
-                    visitedNodes.add(node);
-
-                    const isLastColumn = columnIndex === row.length - 1;
-                    if (isLastColumn) {
-                        continue;
-                    }
-
-                    const nodeOnTheEast = row[columnIndex + 1];
-                    if (nodeOnTheEast.groupId === node.groupId) {
-                        continue;
-                    }
-
-                    node.isWallOnEast = false;
-                    nodeOnTheEast.isWallOnWest = false;
-
-                    const oldid = nodeOnTheEast.groupId!;
-                    nodeOnTheEast.groupId = node.groupId;
-                    this.replace(maze, oldid, node.groupId!)
-                }
-
-                continue
+                continue;
             }
 
-            const groups = new Set();
-            const counts: any = {};
-            for (const [index, node] of row.entries()) {
-                groups.add(node.groupId);
-
-                if (counts[node.groupId!]) {
-                    counts[node.groupId!].count++;
-                    counts[node.groupId!].nodes.push(node);
-                } else {
-                    counts[node.groupId!] = {
-                        count: 1,
-                        nodes: [node]
-                    }
-                }
-            }
-
-            [...groups].forEach(group => {
-                const downWallsCount = JsHelpers.randomInt(1, counts[group as number].count);
-                const nodes = JsHelpers.shuffleArray(counts[group as number].nodes).slice(0, downWallsCount) as ExtendedMazeNode[];
-                nodes.forEach(((node) => {
-                    const nodeOnTheSouth = maze.grid[node.rowIndex + 1][node.columnIndex] as ExtendedMazeNode;
-                    node.isWallOnSouth = false;
-                    nodeOnTheSouth.isWallOnNorth = false;
-                    nodeOnTheSouth.groupId = node.groupId;
-
-                    visitedNodes.add(nodeOnTheSouth);
-                }))
-            })
+            this.buildSouthernPassages(currentRowNodesGroups, extendedMazeGrid, visitedNodes, nodesGroups);
         }
     }
 
-    private replace(maze: Maze, oldGroupId: string, newGroupId: string) {
-        for (const row of maze.grid) {
-            for (const node of (row as ExtendedMazeNode[])) {
-                if (node.groupId === oldGroupId) {
-                    node.groupId = newGroupId;
-                }
+    private buildEasternPassages(nodesGroups: Map<string, NodesGroup>, node: ExtendedMazeNode, columnIndex: number, row: ExtendedMazeNode[], isLastRow: boolean): void {
+        const isLastColumn = columnIndex === row.length - 1;
+        if (isLastColumn) {
+            return;
+        }
+
+        const nodeOnTheEast = row[columnIndex + 1];
+        if (nodeOnTheEast.groupId === node.groupId) {
+            return;
+        }
+
+        // In the last row we need to destroy all walls for distinct groups
+        if (!isLastRow) {
+            const shouldDestroyWallOnEast = JsHelpers.isRandomChanceMet(0.5);
+            if (!shouldDestroyWallOnEast) {
+                return;
             }
+        }
+
+        node.isWallOnEast = false;
+        nodeOnTheEast.isWallOnWest = false;
+
+        this.assignNodesToTheSameGroup(nodesGroups, nodeOnTheEast, node)
+    }
+
+    private addNodeToTheCurrentRowNodesGroup(currentRowNodesGroups: Map<string, NodesGroup>, node: ExtendedMazeNode): void {
+        if (!node.groupId) {
+            throw new Error('Node is missing the group id')
+        }
+
+        if (!currentRowNodesGroups.has(node.groupId)) {
+            currentRowNodesGroups.set(node.groupId, {
+                nodes: [node]
+            });
+            return;
+        }
+
+        currentRowNodesGroups.get(node.groupId)!.nodes.push(node);
+    }
+
+    private buildSouthernPassages(currentRowNodesGroups: Map<string, NodesGroup>, extendedMazeGrid: ExtendedMazeNode[][], visitedNodes: Set<MazeNode>, nodesGroups: Map<string, NodesGroup>): void {
+        for (const [groupId, nodesGroup] of currentRowNodesGroups) {
+            // Draw how many passages needs to be created in the southern direction.
+            // There must be at least one passage
+            const passagesCount = JsHelpers.randomInt(1, nodesGroup.nodes.length);
+
+            // Create passages for the random nodes in the single group
+            const nodes = JsHelpers.shuffleArray(nodesGroup.nodes).slice(0, passagesCount);
+            nodes.forEach(((node) => {
+                const nodeOnTheSouth = extendedMazeGrid[node.rowIndex + 1][node.columnIndex];
+                visitedNodes.add(nodeOnTheSouth);
+
+                node.isWallOnSouth = false;
+                nodeOnTheSouth.isWallOnNorth = false;
+
+                this.assignNodesToTheSameGroup(nodesGroups, nodeOnTheSouth, node);
+            }))
+        }
+    }
+
+    private assignGroupIdForEachNodeInRow(row: ExtendedMazeNode[], groups: Map<string, NodesGroup>): void {
+        for (const node of row) {
+            if (node.groupId) {
+                continue;
+            }
+
+            node.groupId = v4();
+            groups.set(node.groupId, {nodes: [node]});
+        }
+    }
+
+    /**
+     * Assigns all nodes from the first node's group to the second node's group
+     */
+    private assignNodesToTheSameGroup(groups: Map<string, NodesGroup>, node1: ExtendedMazeNode, node2: ExtendedMazeNode): void {
+        if (!node2.groupId) {
+            throw new Error('Second node is missing the group id')
+        }
+
+        const secondGroup = groups.get(node2.groupId);
+        if (!secondGroup) {
+            throw new Error('Second node is not a part of any group')
+        }
+
+        // When using values from the next row, they won't have any group id assigned yet
+        if (node1.groupId) {
+            const firstGroup = groups.get(node1.groupId);
+            if (firstGroup) {
+                groups.delete(node1.groupId);
+                firstGroup.nodes.forEach(node => node.groupId = node2.groupId);
+                secondGroup.nodes.push(...firstGroup.nodes);
+            }
+        } else {
+            node1.groupId = node2.groupId;
+            secondGroup.nodes.push(node1);
         }
     }
 }
