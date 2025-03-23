de60a9654ee15613e46bec3bbed425cec87e1abe
NoxSensei <noxmati@gmail.com>
2025-03-23 19:04:51 +0100

    feat: make the home page responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-        <body className={`${inter.className} bg-zinc-200`}>
+        <body className={`${inter.className} bg-zinc-200 h-full`}>
         <AlgorithmSelectionMenuLayout>
             {children}
         </AlgorithmSelectionMenuLayout>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 'use client'
 
-import ellerComplexExampleImg from "../../public/mazes/eller/complex-example.png";
 import kruskalComplexExampleImg from "../../public/mazes/kruskal/complex-example.png";
-import primComplexExampleImg from "../../public/mazes/prim/complex-example.png";
 import IllustrativeImageComponent from "@/app/algorithms/_common/components/details/illustrative-image";
 
 export default function HomePage() {
-    return <div className="grid grid-cols-3">
-        <div className="col-start-2 flex justify-center pt-20 text-black">
+    return <div className="grid grid-cols-3 h-auto">
+        <div className="col-start-2 flex justify-center pt-[10%] text-black">
             <div className="flex flex-col">
                 <span className="flex justify-center text-4xl">
                     Maze Algorithms
                 </span>
 
-                <p className="pt-20 pb-20 text-justify">
+                <p className="pt-[10%] pb-[10%] text-justify">
                     The goal of the given project is practicing the maze generation algorithms. Inspired by the
                     book &quot;Mazes for Programmers&quot; by Jamis Buck, I have decided to implement my own
                     solution with Next.js. Given idea allowed me to get familiar with the basic maze generation
@@ -21,14 +19,8 @@ export default function HomePage() {
                     detailed descriptions, flow charts and animations making it easier to understand their rules.
                 </p>
 
-                <div className="flex justify-evenly gap-4">
-                    <IllustrativeImageComponent image={kruskalComplexExampleImg} height="30vh"
-                                                alt={'Maze Example'}/>
-
-                    <IllustrativeImageComponent image={ellerComplexExampleImg} height="30vh"
-                                                alt={'Maze Example'}/>
-
-                    <IllustrativeImageComponent image={primComplexExampleImg} height="30vh"
+                <div className="pb-[10%] h-full">
+                    <IllustrativeImageComponent image={kruskalComplexExampleImg} height="100%"
                                                 alt={'Maze Example'}/>
                 </div>
             </div>
