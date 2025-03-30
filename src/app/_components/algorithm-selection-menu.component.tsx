480ed171f6361fc41ac415df23c342f257493eaf
NoxSensei <noxmati@gmail.com>
2025-03-30 20:17:20 +0200

    feat: add hover for home icon

--- src/app/_components/algorithm-selection-menu.component.tsx
+++ src/app/_components/algorithm-selection-menu.component.tsx
@@ -7,15 +7,20 @@ export function AlgorithmSelectionMenuComponent() {
         <nav className="flex flex-col pl-5 pr-5 text-white gap-5">
             <div className="pt-5 pb-5 pl-2">
                 <Link href="/">
-                    <FontAwesomeIcon icon={faHouse} className="h-10"/>
+                    <FontAwesomeIcon icon={faHouse} className="p-2 h-16 hover:bg-zinc-700 hover:rounded-lg"/>
                 </Link>
             </div>
 
-            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2" href="/algorithms/depth-first-search">Depth First Search</Link>
-            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2" href="/algorithms/eller">Eller</Link>
-            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2" href="/algorithms/prim">Prim</Link>
-            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2" href="/algorithms/kruskal">Kruskal</Link>
-            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2" href="/algorithms/hunt-and-kill">Hunt And Kill</Link>
+            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2"
+                  href="/algorithms/depth-first-search">Depth First Search</Link>
+            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2"
+                  href="/algorithms/eller">Eller</Link>
+            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2"
+                  href="/algorithms/prim">Prim</Link>
+            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2"
+                  href="/algorithms/kruskal">Kruskal</Link>
+            <Link className="hover:bg-zinc-700 hover:rounded-lg pt-1 pb-1 pl-2 pr-2"
+                  href="/algorithms/hunt-and-kill">Hunt And Kill</Link>
         </nav>
     )
 }
