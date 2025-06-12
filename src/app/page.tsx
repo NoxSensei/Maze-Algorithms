ba3eefdce311934a579440719e5db34e66e4948e
Coding Unicorn <noxmati@gmail.com>
2025-06-12 23:51:13 +0200

    feat: update homepage description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,58 @@ export default function HomePage() {
                     detailed descriptions, flow charts and animations making it easier to understand their rules.
                 </p>
 
-                <div className="pb-[5%] h-full">
+                <p className="pb-[5%] text-justify">
+                    In my implementation I have been using approach where the grid is already generated and algorithm
+                    is building passages by destroying the walls. If you got a moment take a look into the useful
+                    links section as part of the algorithms starts with all walls placed, some of them with empty grid
+                    and
+                    there are ones that can work in two modes. There are also other cool classifications worth seeing.
+                </p>
+
+                <div className="pb-[5%]">
                     <IllustrativeImageComponent image={kruskalComplexExampleImg} width="60%"
                                                 alt={'Maze Example'}/>
                 </div>
+
+                <div className="py-10 px-6 rounded-2xl">
+                    <h2 className="text-2xl font-semibold text-center text-black mb-6">
+                        Useful Links
+                    </h2>
+                    <ul className="list-disc list-inside space-y-4 text-justify marker:text-black">
+                        <li>
+                            <a
+                                href="https://www.jamisbuck.org/mazes/"
+                                className="text-blue-600 dark:text-blue-400 hover:underline font-medium"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Interactive mazes with examples by Jamis Bucks
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.astrolog.org/labyrnth/algrithm.htm"
+                                className="text-blue-600 dark:text-blue-400 hover:underline font-medium"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Maze Classification by Walter D. Pullen
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.astrolog.org/labyrnth/daedalus.htm"
+                                className="text-blue-600 dark:text-blue-400 hover:underline font-medium"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Software for generating mazes – Daedalus
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </div>
-    </div>;
-
+    </div>
 }
